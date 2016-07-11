e8f64541f0ad1ed774d88ff8cced4bf142d716ba
Abhishek Sharma <abhisheks1712@gmail.com>
2016-07-11 14:08:33 +0530

    Add database migrations for voters and colonies tables

---
+++ database/migrations/2016_07_11_082749_create_voters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('serial_num')->unique();
+            $table->string('name', 100);
+            $table->string('fathers_name', 100)->nullable();
+            $table->string('epic_number', 30)->unique();            
+            $table->char('mobile', 10);
+            $table->string('colony', 100);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('voters');
+    }
+}
